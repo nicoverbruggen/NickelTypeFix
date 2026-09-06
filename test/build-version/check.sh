42826fc0117ad4f9b949618f2158557ef254a8cc
Nico Verbruggen <mail@nicoverbruggen.be>
2026-09-06 17:28:54 +0200

    Fix source-archive version check in Git checkouts

--- test/build-version/check.sh
+++ test/build-version/check.sh
@@ -16,5 +16,9 @@ version() {
 test "$(version VERSION=v9.9 NTF_DEV_BUILD=0)" = v9.9
 test "$(version VERSION=v9.9 NTF_DEV_BUILD=1)" = "v9.9-dev-$fingerprint"
 test "$(version VERSION=v9.9-7-g1234567-dirty NTF_DEV_BUILD=1)" = "v9.9-7-g1234567-dirty-dev-$fingerprint"
-test "$(GIT_DIR="$work/no-git" version VERSION= NTF_DEV_BUILD=1)" = "dev-$fingerprint"
+# Export through the shell function so make's Git lookup cannot see the checkout.
+test "$(
+    export GIT_DIR="$work/no-git"
+    version VERSION= NTF_DEV_BUILD=1
+)" = "dev-$fingerprint"
 echo 'build version tests passed'
